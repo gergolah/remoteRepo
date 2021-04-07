0f6f0daac6478266a6dad278e9fc9608d10f2147
gergely.olah <gergolah@gmail.com>
2021-04-07 18:13:22 +0200

    kerdesek

---
+++ GitProba/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //file beolvasás és tárolás
+            //adatok száma
+            //legnagyobb elem helye
+            //van budapesti lakos?
+            //mindenki 18 feletti?
+            //ki lakik a XIII. kerületben?
+            //milyen kerületekben laknak?
+            //melyik kerületben hányan laknak?
+        }
+    }
+}
