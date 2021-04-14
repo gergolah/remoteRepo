9a311619664dd3f9fd806ad82a6cd45c8209f1ce
gergolah <gergolah@gmail.com>
2021-04-14 16:47:20 +0200

    üzenet

--- GitProba/Program.cs
+++ GitProba/Program.cs
@@ -39,7 +39,26 @@ namespace GitProba
 
             //van budapesti lakos?
 
+            bool vanBudapestiLakos = false;
+            for (int i = 0; i < N; i++)
+            {
+                if (osszesLakos[i].GetHely() == "Budapest")
+                {
+                    vanBudapestiLakos = true;
+                }
+            }
+            string kiiras = vanBudapestiLakos ? "van" : "nincs";
+            //Console.WriteLine($"{kiiras} budapesti lakos");
+
             //mindenki 18 feletti?
+
+            int j = 0;
+            while (j < N && osszesLakos[j].GetKor() > 18)
+            {
+                j++;
+            }
+            bool mindenki18Feletti = j >= N;
+
 
             //ki lakik a XIII. kerületben?
 
@@ -64,6 +83,42 @@ namespace GitProba
             }
 
             //melyik kerületben hányan laknak?
+
+            Dictionary<string, int> holHanyanLaknak = new Dictionary<string, int>();
+            foreach (var lakos in osszesLakos)
+            {
+                string kulcs = lakos.GetKerulet();
+                if (kulcs == "")
+                {
+                    kulcs = "Nem budapesti";
+                }
+                if (holHanyanLaknak.ContainsKey(kulcs))
+                {
+                    holHanyanLaknak[kulcs]++;
+                }
+                else
+                {
+                    holHanyanLaknak.Add(kulcs, 1);
+                }
+            }
+            foreach (var item in holHanyanLaknak)
+            {
+                Console.WriteLine($"{item.Key} kerületben ennyien laknak: {item.Value}");
+            }
+
+            //írjuk ki fileba a három legidősebb lakost!
+            var lakosokSorrendbenLista = osszesLakos.OrderByDescending(x => x.GetKor()).ToList();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(lakosokSorrendbenLista[i].GetNev());
+            }
+            //string tömbbe írás
+            //int tombHossz = 3;
+            //string[] lakosokSorrendbenTomb = lakosokSorrendbenLista.Select(c => c.ToString()).ToArray();
+            //for (int i = 0; i < tombHossz; i++)
+            //{
+            //    File.WriteAllLines("harom_legidosebb_lakos.txt", lakosokSorrendbenTomb);
+            //}
 
             Console.ReadLine();
         }
